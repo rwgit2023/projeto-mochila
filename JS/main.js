c6316dce6c18ae6c786fa44f159d249083e31d7b
rwgit2023 <ronangit2023@hotmail.com>
2023-02-27 17:00:54 -0300

    Fourth Commit

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,26 @@
 const form = document.querySelector('#novoItem')
 
 form.addEventListener("submit", function(elemento){
+
     elemento.preventDefault()  // Todo formulário por padrão tem que enviar os dados pra algum lugar, porém estou enviando pra minha prórpia página... entao tenho que tratar o erro que vai dar.
     // console.log(elemento)
     // console.log(elemento.target[0].value)    //pego o elemento no input 0 (Nome)
-    criaElemento(elemento.target.elements['nome'].value)
-    criaElemento(elemento.target.elements['quantidade'].value) // pegando pela nomenclatura do elemento fica mais confiável
+    criaElemento(elemento.target.elements['nome'].value, elemento.target.elements['quantidade'].value) // pegando pela nomenclatura do elemento fica mais confiável
+    
 })
 
 function criaElemento (nome, quantidade){
 
-console.log(nome,quantidade)
-}
+    console.log(nome)
+    console.log(quantidade)
+
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement('Strong')
+    numeroItem.innerHTML=quantidade
+
+    novoItem.innerHTML = numeroItem + nome
+
+    console.log(numeroItem)
+} 
